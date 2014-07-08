93ca51d712d43061358d5277789ae078aeb3ccd1
geomel <geomel@gmail.com>
2014-07-08 15:27:10 +0300

    Refactoring. Cleaned up dashboard.php page and extracted js code to its own file

--- dashboard.php
+++ dashboard.php
@@ -353,14 +353,6 @@ include("inc/nav.php");
 				</div>		
 			</div>		
 	</div>
-
-	
-	
-	
-	
-	
-	
-	
 	
 </div>	
 	
@@ -375,139 +367,10 @@ include("inc/nav.php");
 <script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.resize.js"></script>
 <script src="<?php echo ASSETS_URL; ?>/js/plugin/flot/jquery.flot.tooltip.js"></script>
 
-<!-- Vector Maps Plugin: Vectormap engine, Vectormap language -->
-<script src="<?php echo ASSETS_URL; ?>/js/plugin/vectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="<?php echo ASSETS_URL; ?>/js/plugin/vectormap/jquery-jvectormap-world-mill-en.js"></script>
-
-<!-- Full Calendar -->
-		<!-- PAGE RELATED PLUGIN(S) -->
-		<!-- PAGE RELATED PLUGIN(S) -->
 <script src="<?php echo ASSETS_URL; ?>/js/plugin/datatables/jquery.dataTables-cust.min.js"></script>
-<script src="<?php echo ASSETS_URL; ?>/js/plugin/datatables/ColReorder.min.js"></script>
-<script src="<?php echo ASSETS_URL; ?>/js/plugin/datatables/FixedColumns.min.js"></script>
-<script src="<?php echo ASSETS_URL; ?>/js/plugin/datatables/ColVis.min.js"></script>
-<script src="<?php echo ASSETS_URL; ?>/js/plugin/datatables/ZeroClipboard.js"></script>
 <script src="<?php echo ASSETS_URL; ?>/js/plugin/datatables/media/js/TableTools.min.js"></script>
-<script src="<?php echo ASSETS_URL; ?>/js/plugin/datatables/DT_bootstrap.js"></script>
-
-<script src="<?php echo ASSETS_URL; ?>/js/plugin/fullcalendar/jquery.fullcalendar.min.js"></script>
-<script type="text/javascript">
-	$(document).ready(function() {
-		 refreshTimeLine();
-		 
-		 
-		 //------------------------ DATATABLE TOOLS --------------------------
-		 /* Add the events etc before DataTables hides a column */
-		$("#datatable_fixed_column thead input").keyup(function() {
-			oTable.fnFilter(this.value, oTable.oApi._fnVisibleToColumnIndex(oTable.fnSettings(), $("thead input").index(this)));
-		});
-
-		$("#datatable_fixed_column thead input").each(function(i) {
-			this.initVal = this.value;
-		});
-		$("#datatable_fixed_column thead input").focus(function() {
-			if (this.className == "search_init") {
-				this.className = "";
-				this.value = "";
-			}
-		});
-		$("#datatable_fixed_column thead input").blur(function(i) {
-			if (this.value == "") {
-				this.className = "search_init";
-				this.value = this.initVal;
-			}
-		});		
-		
-
-		var oTable = $('#datatable_fixed_column').dataTable({
-			"sDom" : "<'dt-top-row'><'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
-			//"sDom" : "t<'row dt-wrapper'<'col-sm-6'i><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'>>",
-			"oLanguage" : {
-				"sSearch" : "Search all columns:"
-			},
-			"bSortCellsTop" : true
-		});		
-		
-
-
-		/*
-		 * COL ORDER
-		 */
-		$('#datatable_col_reorder').dataTable({
-			"sPaginationType" : "bootstrap",
-			"sDom" : "R<'dt-top-row'Clf>r<'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
-			"fnInitComplete" : function(oSettings, json) {
-				$('.ColVis_Button').addClass('btn btn-default btn-sm').html('Columns <i class="icon-arrow-down"></i>');
-			}
-		});
-		
-		/* END COL ORDER */
-
-		/* TABLE TOOLS */
-		$('#datatable_tabletools').dataTable({
-			"sDom" : "<'dt-top-row'Tlf>r<'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
-			"oTableTools" : {
-				"aButtons" : ["copy", "print", {
-					"sExtends" : "collection",
-					"sButtonText" : 'Save <span class="caret" />',
-					"aButtons" : ["csv", "xls", "pdf"]
-				}],
-				"sSwfPath" : "<?php echo ASSETS_URL; ?>/js/plugin/datatables/media/swf/copy_csv_xls_pdf.swf"
-			},
-			"fnInitComplete" : function(oSettings, json) {
-				$(this).closest('#dt_table_tools_wrapper').find('.DTTT.btn-group').addClass('table_tools_group').children('a.btn').each(function() {
-					$(this).addClass('btn-sm btn-default');
-				});
-			}
-		});
-		
-			/* TABLE TOOLS */
-		$('#commitersMetricsTable').dataTable({
-			"sDom" : "<'dt-top-row'Tlf>r<'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
-			"oTableTools" : {
-				"aButtons" : ["copy", "print", {
-					"sExtends" : "collection",
-					"sButtonText" : 'Save <span class="caret" />',
-					"aButtons" : ["csv", "xls", "pdf"]
-				}],
-				"sSwfPath" : "<?php echo ASSETS_URL; ?>/js/plugin/datatables/media/swf/copy_csv_xls_pdf.swf"
-			},
-			"fnInitComplete" : function(oSettings, json) {
-				$(this).closest('#dt_table_tools_wrapper').find('.DTTT.btn-group').addClass('table_tools_group').children('a.btn').each(function() {
-					$(this).addClass('btn-sm btn-default');
-				});
-			}
-		});
-		
-		$('#softMetricsTable').dataTable({
-			"sDom" : "<'dt-top-row'Tlf>r<'dt-wrapper't><'dt-row dt-bottom-row'<'row'<'col-sm-6'i><'col-sm-6 text-right'p>>",
-			"oTableTools" : {
-				"aButtons" : ["copy", "print", {
-					"sExtends" : "collection",
-					"sButtonText" : 'Save <span class="caret" />',
-					"aButtons" : ["csv", "xls", "pdf"]
-				}],
-				"sSwfPath" : "<?php echo ASSETS_URL; ?>/js/plugin/datatables/media/swf/copy_csv_xls_pdf.swf"
-			},
-			"fnInitComplete" : function(oSettings, json) {
-				$(this).closest('#dt_table_tools_wrapper').find('.DTTT.btn-group').addClass('table_tools_group').children('a.btn').each(function() {
-					$(this).addClass('btn-sm btn-default');
-				});
-			}
-		});
-
-	/* END TABLE TOOLS */
-		 
-	})
-	
-	function refreshTimeLine(){
-        $('#ajax-timeline').load('_/php/_timeline.php', function(){		
-           // setTimeout(refreshTimeLine, 5000);
-        });
-		
-    }
-</script>		
-
+<script src="<?php echo ASSETS_URL; ?>/js/plugin/datatables/DT_bootstrap.js"></script>	
+<script src="_/js/_dashboard.js"></script>
 
 <?php 
 	//include footer
