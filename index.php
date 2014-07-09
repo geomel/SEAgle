2d5aa9ac7ec9efa86cfa44decc32b143401ee012
geomel <geomel@gmail.com>
2014-07-09 18:30:43 +0300

    Changes in the index.php, search filters functionality in progress

--- index.php
+++ index.php
@@ -8,8 +8,14 @@ $page_body_prop = array("id"=>"login", "class"=>"animated fadeInDown");
 include("inc/header.php");
 
 ?>
-        <div class="navbar-collapse navbar-right collapse">
-			 <a href='javascript:void(0)' class='btn btn-sm btn-default' id="showTimeline" style="margin-top:10px; margin-right:20px"><i class='fa fa-arrow-down text-muted'></i> TIMELINE OVERVIEW</a>	
+	<div style="padding:20px; margin-top:10px">	
+		<button type="button" class="btn btn-labeled btn-success btn-lg" data-toggle="modal" data-target="#myModal">
+			<i class="fa fa-video-camera"></i>
+				Instructional Video
+		</button>	
+	</div>	
+        <div class="pull-right">
+			 <a href='javascript:void(0)' class='btn btn-sm btn-default' id="showTimeline" style="margin-top:-50px; margin-right:20px"><i class='fa fa-arrow-down text-muted'></i> TIMELINE OVERVIEW</a>	
 						<!-- Timeline Content -->
 						<div class='smart-timeline'>	
 							<ul class='smart-timeline-list'>
@@ -17,15 +23,8 @@ include("inc/header.php");
 						</div>
 					<!-- END Timeline Content -->		
         </div>
-	<div class="row" style="padding:20px">	
-		<button type="button" class="btn btn-labeled btn-success btn-lg" data-toggle="modal" data-target="#myModal">
-			<i class="fa fa-video-camera"></i>
-				Instructional Video
-		</button>	
-	</div>	
-
 	<div class="col-lg-6 col-lg-offset-3">
-		<a href="index.php"><img src="img/seanets_logo_big.png" alt="SEAgle" class="img-responsive center-block" width="450" height="200" style="padding-top:100px; margin-bottom:0px;"></a>	
+		<a href="index.php"><img src="img/seanets_logo_big.png" alt="SEAgle" class="img-responsive center-block" width="450" height="200" style="padding-top:20px; margin-bottom:20px;"></a>	
 	</div>
 <!-- ==========================CONTENT STARTS HERE ========================== -->
 <!-- MAIN PANEL -->
@@ -40,9 +39,27 @@ include("inc/header.php");
 							<button type="submit" class="btn btn-default" id="search-button">
 									&nbsp;&nbsp;&nbsp;<i class="fa fa-fw fa-search fa-lg"></i>&nbsp;&nbsp;&nbsp;
 							</button>
-						</span>
+						
 					</div><!-- /input-group -->
 				</div><!-- /.col-lg-4 -->
+				<div class="col-lg-6 col-lg-offset-3">
+					<form class="smart-form" style="margin-top:10px;">	
+							<section>
+											<label class="text-muted">Search Filter</label>
+											<div class="inline-group">
+												<label class="radio">
+													<input type="radio" name="radio-inline" checked="checked">
+													<i></i>All</label>
+												<label class="radio">
+													<input type="radio" name="radio-inline">
+													<i></i>Number of Versions</label>
+												<label class="radio">
+													<input type="radio" name="radio-inline">
+													<i></i>TimeLine</label>
+											</div>
+							</section>
+						</form>	
+					</div>	
 						<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12' style="margin-left:30px;">	
 								<span class="note"><span id="results">  </span>About <span id="execsqltime" style="margin-top:10px;"/> </span>
 							<div id="search-res"></div>	
